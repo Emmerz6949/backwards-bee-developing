906efa5fd8290b68c1d1e02a29ee3b45e001b3cb
emmerz6949 <emmerz6949@gmail.com>
2020-11-16 16:57:47 -0500

    updated 'class' to 'className'

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,24 +4,24 @@ function Header() {
     return(
         // <!--header containing navigation bar-->
         <header>
-            <nav class="navbar navbar-expand-lg navbar-dark cerulean">
-                <h1 class="navbar-brand mb-0"><a class="text-decoration-none text-reset" href="../index.html">Emmalee Barnett</a></h1>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-dark cerulean">
+                <h1 className="navbar-brand mb-0"><a className="text-decoration-none text-reset" href="../index.html">Emmalee Barnett</a></h1>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/">Home</a>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/about">About Me</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">About Me</a>
                         </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="/portfolio">My Portfolio</a>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/portfolio">My Portfolio</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/contact">Contact Me</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/contact">Contact Me</a>
                         </li>
                     </ul>
                 </div>

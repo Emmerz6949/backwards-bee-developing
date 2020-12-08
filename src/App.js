7965e66d236b5cc33c539fb01da9cbc7fa7c519b
emmerz6949 <emmerz6949@gmail.com>
2020-12-08 16:43:40 -0500

    updated the imports for App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import './style.css';
+import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
 
 function App() {
   return (

40b674624bc0f75a47aa2f06577086d4886d0cca
emmerz6949 <emmerz6949@gmail.com>
2020-12-08 16:21:35 -0500

    added some classes for styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 function Project(props) {
-    let classN = `card text-center border-success my-2 ${props.rowInfo}`;
+    let classN = `card text-center text-light bg-dark bord my-2 ${props.rowInfo}`;
     
     return(
         <div className={classN}>
-            <img src={props.imgSrc} className="card-img-top border-success" alt="screenshot of the main page for this site"/>
-            <div className="card-body border-success">
+            <img src={props.imgSrc} className="card-img-top bord" alt="screenshot of the main page for this site"/>
+            <div className="card-body bord">
                 <h5 className="card-title">{props.title}</h5>
                 <a href={props.repository} target="_blank" rel="noopener noreferrer" className="btn cerulean text-light rounded-0 m-1">Repository</a>
                 <a href={props.deployed} target="_blank" rel="noopener noreferrer" className="btn cerulean text-wrap text-light rounded-0 m-1">Deployed Site</a>

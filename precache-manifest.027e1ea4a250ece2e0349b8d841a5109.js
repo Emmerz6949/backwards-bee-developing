44eeda9c4ee0abec194f21eef2dc0ddad9b3e3da
Emmerz6949 <emmerz6949@gmail.com>
2021-01-15 22:03:09 -0500

    Updates

---
+++ precache-manifest.027e1ea4a250ece2e0349b8d841a5109.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd93ea454b9ed7c3bf14957fd29eddb",
+    "url": "/backwards-bee-developing/index.html"
+  },
+  {
+    "revision": "c4ae2800c906be25cf69",
+    "url": "/backwards-bee-developing/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b203244fd3a4e04c094a",
+    "url": "/backwards-bee-developing/static/css/main.78ab1b01.chunk.css"
+  },
+  {
+    "revision": "c4ae2800c906be25cf69",
+    "url": "/backwards-bee-developing/static/js/2.0f6a84bd.chunk.js"
+  },
+  {
+    "revision": "44b6ea107d5f6954b31204b3600b6c0e",
+    "url": "/backwards-bee-developing/static/js/2.0f6a84bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b203244fd3a4e04c094a",
+    "url": "/backwards-bee-developing/static/js/main.7ad29a1d.chunk.js"
+  },
+  {
+    "revision": "d0a1591a7acc5f8cf6d4",
+    "url": "/backwards-bee-developing/static/js/runtime-main.fece3226.js"
+  },
+  {
+    "revision": "b47ea81ff5352c94b7fd078cc689d174",
+    "url": "/backwards-bee-developing/static/media/Emmalee Barnett's Resume.doc.b47ea81f.pdf"
+  },
+  {
+    "revision": "cdd9310ed24197d244b83f6ce374e057",
+    "url": "/backwards-bee-developing/static/media/myself.cdd9310e.jpg"
+  }
+]);
